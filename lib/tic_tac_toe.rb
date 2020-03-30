b950b12074b3fd07d5378d5380669792a63d77f9
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-30 21:18:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,7 @@ class TicTacToe
   ]
 
   # displays the current state of the board
-  def display_board)
+  def display_board
   row1 = " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   row2 = " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   row3 = " #{@board[6]} | #{@board[7]} | #{@board[8]} "
