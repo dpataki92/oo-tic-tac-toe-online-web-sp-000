a42307282afe9cf32db02faf87000520ac341f2d
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-30 21:16:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,11 +5,11 @@ class TicTacToe
   end
 
   WIN_COMBINATIONS = [
-    [0,1,2] #top row
-    [3,4,5] #middle row
-    [6,7,8] #bottom row
-    [0,4,8] #diagonal from top left corner
-    [2,4,6] #diagonal from top right corner
+    [0,1,2], #top row
+    [3,4,5], #middle row
+    [6,7,8], #bottom row
+    [0,4,8], #diagonal from top left corner
+    [2,4,6], #diagonal from top right corner
     [0,3,6], #left column
     [1,4,7], #middle column
     [2,5,8] #right column
